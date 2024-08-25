4adbdfaaa4765b0c1c4b8c64a89cda2995d48235
dankfrank7 <tomryan4811@gmail.com>
2024-08-25 19:38:20 +1000

    25/08/2024

---
+++ Algorithms1/Week5/KdTree.java
@@ -0,0 +1,161 @@
+package Week5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+public class KdTree {
+    private static final boolean VERTICAL = true;       // x-coordinate
+    private static final boolean HORIZONTAL = false;    // y-coordinate
+    private Node root;
+    private int size; 
+
+    private class Node {
+        Point2D p; 
+        Node left, right;
+        boolean dim; // current dimension (VERTICAL or HORIZONTAL)
+
+        public Node(Point2D p, boolean dim) {
+            this.p = p;
+            this.left = null;
+            this.right = null;
+            this.dim = dim; // true if vertical, false if 
+        }
+    }
+
+    // construct an empty set of points
+    public KdTree() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    // is the set empty? 
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
+    // number of points in the set
+    public int size() {
+        return size;
+    }
+
+    // add a point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("KdTree.insert: argument Point2D p is null");
+        this.root = insert(root, p, VERTICAL);  
+    }
+
+    private Node insert(Node node, Point2D p, boolean dim) {
+        if (node == null) {
+            this.size++;
+            return new Node(p, VERTICAL); // start with vertical
+        }
+        int cmp;
+        if (dim == VERTICAL) cmp = Point2D.X_ORDER.compare(p, node.p); // vertical
+        else                 cmp = Point2D.Y_ORDER.compare(p, node.p); // horizontal
+        
+        if      (cmp < 0) node.left = insert(node.left, p, !node.dim);
+        else if (cmp > 0) node.right = insert(node.right, p, !node.dim);
+        else if (!node.p.equals(p)) node.right = insert(node.right, p, !node.dim); // handle duplicate points
+    
+        return node;
+    }
+
+    // does the set contain p?
+    public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("KdTree.contains: Point2D p is null");
+        return contains(root, p, VERTICAL);
+    }
+
+    private boolean contains(Node node, Point2D p, boolean dim) {
+        if (node == null) return false;
+        if (node.p.equals(p)) return true;
+    
+        int cmp;
+        if (dim == VERTICAL) cmp = Point2D.X_ORDER.compare(p, node.p); // vertical
+        else                 cmp = Point2D.Y_ORDER.compare(p, node.p); // horizontal
+    
+        if (cmp < 0) return contains(node.left, p, !dim);
+        else return contains(node.right, p, !dim);
+    }
+
+    //  draw all points to standard draw
+    public void draw() {
+        draw(root);
+    }
+
+    private void draw(Node node) {
+        if (node == null) return;
+        node.p.draw();
+        draw(node.left);
+        draw(node.right);
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException("KdTree.range: RectHV rect is null");
+        List<Point2D> inside = new ArrayList<>();
+        range(root, inside, rect);
+        return inside;
+    }
+
+    private void range(Node node, List<Point2D> inside, RectHV rect) {
+        if (node == null) return;
+        if (rect.contains(node.p)) inside.add(node.p);
+    
+        RectHV leftRect, rightRect;
+        if (node.dim == VERTICAL) {
+            leftRect = new RectHV(rect.xmin(), rect.ymin(), node.p.x(), rect.ymax());
+            rightRect = new RectHV(node.p.x(), rect.ymin(), rect.xmax(), rect.ymax());
+        } else {
+            leftRect = new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), node.p.y());
+            rightRect = new RectHV(rect.xmin(), node.p.y(), rect.xmax(), rect.ymax());
+        }
+    
+        if (rect.intersects(leftRect)) {
+            range(node.left, inside, rect);
+        }
+    
+        if (rect.intersects(rightRect)) {
+            range(node.right, inside, rect);
+        }
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (isEmpty()) return null;
+        if (p == null) throw new IllegalArgumentException("KdTree.nearest: Point2D p is null");
+        return nearest(root, p, root.p, VERTICAL);
+    }
+
+    private Point2D nearest(Node node, Point2D p, Point2D nearest, boolean dim) {
+        if (node == null) return nearest; 
+        if (node.p.distanceSquaredTo(p) < nearest.distanceSquaredTo(p)) {
+            nearest = node.p;
+        }
+        Node first, second;
+        if ((dim == VERTICAL && p.x() < node.p.x()) || (dim == HORIZONTAL && p.y() < node.p.y())) {
+            first = node.left;
+            second = node.right;
+        } else {
+            first = node.right;
+            second = node.left;
+        }
+    
+        nearest = nearest(first, p, nearest, !dim);
+        if (second != null && 
+            ((dim == VERTICAL && Math.abs(p.x() - node.p.x()) < nearest.distanceSquaredTo(p)) || 
+             (dim == HORIZONTAL && Math.abs(p.y() - node.p.y()) < nearest.distanceSquaredTo(p)))) {
+            nearest = nearest(second, p, nearest, !dim);
+        }
+        return nearest;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Algorithms1/Week5/PointSET.java
@@ -0,0 +1,79 @@
+package Week5; 
+
+import java.util.*;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+    private SET<Point2D> points;
+
+    // construct an empty set of points
+    public PointSET() {
+        this.points = new SET<>();
+    }
+
+    // is the set empty? 
+    public boolean isEmpty() {
+        return this.points.size() == 0;
+    }
+
+    // number of points in the set
+    public int size() {
+        return this.points.size();
+    }
+
+    // add a point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("PointSET.insert: Point2D p is null");
+        this.points.add(p);
+    }
+
+    // does the set contain p?
+    public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException("PointSET.contains: Point2D p is null");
+        return this.points.contains(p);
+    }
+
+    //  draw all points to standard draw
+    public void draw() {
+        if (this.points.size() == 0) return;
+        for (Point2D point : points) {
+            point.draw();
+        }        
+     }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException("PointSET.range: RectHV rect is null");
+        List<Point2D> inside = new ArrayList<>();
+        for (Point2D point : this.points) {
+            if (rect.contains(point)) {
+                inside.add(point);
+            }
+        }
+        return inside;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (this.points.isEmpty()) return null;
+        if (p == null) throw new IllegalArgumentException("PointSET.nearest: Point2D p is null");
+
+        Point2D nearest = null;
+        double nearestDist = Double.MAX_VALUE;
+
+        for (Point2D point : points) {
+            double dist = p.distanceTo(point);
+            if (dist < nearestDist) {
+                nearestDist = dist;
+                nearest = point;
+            }
+        }
+        return nearest;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
